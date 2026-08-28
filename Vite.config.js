baa8e4548de649ea01a99b84f247b6ac5fa5f5e5
alexemmanuel <alexemmanuelz24@gmail.com>
2026-08-28 13:28:47 +0100

    Delete Vite.config.js

--- Vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  // GitHub project pages: base: "/chain-stories/"
-  // Custom domain or username.github.io root: base: "/"
-  base: "/",
-});
