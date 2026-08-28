5f2bd5d5fe334cbf6adfb3267a12553d25028260
alexemmanuel <alexemmanuelz24@gmail.com>
2026-08-28 15:16:35 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // GitHub project pages example: base: "/chain-stories/"
+  // GitHub project pages: base: "/chain-stories/"
+  // Custom domain or username.github.io root: base: "/"
   base: "/",
 });
