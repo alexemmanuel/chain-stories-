18d6c545263cc90aac147b9705f7dff69fdc1896
alexemmanuel <alexemmanuelz24@gmail.com>
2026-08-29 21:30:25 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // Must match the repo name exactly (including trailing slash)
   base: "/chain-stories-/",
 });
